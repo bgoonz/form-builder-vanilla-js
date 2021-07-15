fe926a53bb859de4ba83ed45f1680ffde190ff18
bryan <bryan.guner@gmail.com>
2021-07-15 19:53:17 -0400

    update

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+require.config({
+  baseUrl: "assets/js/lib/",
+  shim: {
+    backbone: {
+      deps: ["underscore", "jquery"],
+      exports: "Backbone",
+    },
+    popper: {
+      exports: "Popper",
+    },
+    underscore: {
+      exports: "_",
+    },
+    bootstrap: {
+      deps: ["jquery"],
+      exports: "$.fn.popover",
+    },
+  },
+  paths: {
+    app: "..",
+    collections: "../collections",
+    data: "../data",
+    models: "../models",
+    helper: "../helper",
+    templates: "../templates",
+    views: "../views",
+  },
+});
+
+require(["popper.min", "app/app"], function (popper, app) {
+  app.initialize();
+});
